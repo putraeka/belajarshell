5dc9d5249fe8691be902f1d3ae058683485cffb2
putraeka <gh@eka.gdn>
2019-03-24 21:45:54 +0700

    push pertama - belajar echo, variable dan simple if

---
+++ myscript.sh
@@ -0,0 +1,21 @@
+#! /bin/bash
+
+# echo command
+# echo Hello World!
+
+# Variables
+# Uppercase by convention
+# Letters, numbers, underscores
+NAME="Brad"
+# echo "My Name is $NAME"
+# echo "My Name is ${NAME}"
+
+# User Input
+# read -p "Enter your name: " NAME
+# echo "Hello $NAME, nice to meet you!"
+
+# Simple If Statement
+if [ "$NAME" == "Brad" ]
+then
+    echo "Your name is Brad"
+fi
